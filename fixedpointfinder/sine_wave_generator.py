15d043e1c306e48f6717a9ddb1a50b4e1b749b96
prstolpe <pr.stolpe@gmail.com>
2020-05-10 20:31:14 +0200

    Checked for fixed points. Everything working fine

--- fixedpointfinder/sine_wave_generator.py
+++ fixedpointfinder/sine_wave_generator.py
@@ -5,6 +5,7 @@ from utilities.model_utils import build_sub_model_to
 from tensorflow.keras.models import load_model
 import os
 from tensorflow.python.keras.utils import tf_utils
+from mpl_toolkits.mplot3d import Axes3D
 
 
 class SineWaveGenerator(object):
@@ -20,7 +21,7 @@ class SineWaveGenerator(object):
         self.name = 'sine_generator'
         self.verbose = True
 
-        self.data_hps = {'n_batch': 150,
+        self.data_hps = {'n_batch': 100,
                          'n_time': 5,
                          'dt': 0.01}
         self.n_time = 150
@@ -28,8 +29,8 @@ class SineWaveGenerator(object):
         self.frequency_fun = lambda x: 0.04 * x + 0.01
 
         self.rng = npr.RandomState(125)
-        self.n_frequencies = 3
-        self.frequencies = np.linspace(0.2, 0.6, self.n_frequencies)
+        self.n_frequencies = 1
+        self.frequencies = np.linspace(0.4, 0.6, self.n_frequencies)
 
         self.model = self._build_model()
 
@@ -94,6 +95,12 @@ class SineWaveGenerator(object):
                                  tf.convert_to_tensor(stimulus['output'], dtype=tf.float32), epochs=n_epochs)
         return history
 
+    def get_activations(self, stim):
+        sub_model = build_sub_model_to(self.model, [self.rnn_type])
+        activation = sub_model.predict(tf.convert_to_tensor(stim['input'], dtype=tf.float32))
+
+        return activation
+
 
 if __name__ == "__main__":
 
@@ -103,7 +110,7 @@ if __name__ == "__main__":
     sinwaver = SineWaveGenerator(rnn_type=rnn_type, n_hidden=n_hidden)
 
     stimulus = sinwaver.generate_stimulus()
-    history = sinwaver.train(stimulus, 600)
+    history = sinwaver.train(stimulus, 400)
     import matplotlib.pyplot as plt
 
     prediction = sinwaver.model.predict(tf.convert_to_tensor(stimulus['input'], dtype=tf.float32))
@@ -114,3 +121,15 @@ if __name__ == "__main__":
 
     #plt.plot(stimulus['input'][0, :, :])
     #plt.show()
+
+    import sklearn.decomposition as skld
+
+    activation = sinwaver.get_activations(stimulus)
+    pca = skld.PCA(3)
+    x_pca = pca.fit_transform(activation[0, :, :])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    n_points = 150
+    ax.plot(x_pca[:n_points, 0], x_pca[:n_points, 1], x_pca[:n_points, 2],
+            linewidth=0.7)
